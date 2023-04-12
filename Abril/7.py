684da986770584bcf2fd250d2922caa61308b2fd
cauan2305 <cauanmatheustrigo68@gmail.com>
2023-04-12 20:34:41 -0300

    resolved exercise 7

---
+++ Abril/7.py
@@ -0,0 +1,11 @@
+def get_count(sentence: str):
+   return len([word for word in sentence if word in ["a", "e", "i", "o", "u"]])
+"""
+    Return the number (count) of vowels in the given string.
+
+    We will consider a, e, i, o, u as vowels for this Kata (but not y).
+
+    The input string will only consist of lower case letters and/or spaces.
+"""
+
+print(get_count("aeiou"))
